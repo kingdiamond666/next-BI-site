ea4bd35a1d66c8d9f810df8c7f13d9ec308d9027
Christian Collins <christiancollins743@gmail.com>
2021-11-09 15:13:49 -0800

    Update Tailwind Config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,36 @@
-// tailwind.config.js
+// // tailwind.config.js
+// module.exports = {
+//   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//   ],
+// }
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  // eslint-disable-next-line global-require
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 }
